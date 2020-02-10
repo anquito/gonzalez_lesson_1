c698a36d13b0ee29e412045f412999e6b79e9d91
lorena31aldana <Vampfrek13!>
2020-02-10 10:32:49 -0800

    Lesson 1.C

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float verticalSpeed = 20;
+    public float horizontalSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward);
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalSpeed * verticalInput);
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //transform.Translate(Vector3.right * Time.deltaTime * horizontalSpeed * horizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * horizontalSpeed * horizontalInput);
     }
 }
